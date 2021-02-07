0ab4c6214fafde0c88b304d5e94ce0b97d636124
EvanSarwer <evansarwer1@gmail.com>
2021-02-07 16:57:09 +0600

    php if loops prblm 1,2

---
+++ Lab_Tasks/Lab_Task-3/PHP_if_loops/Problem-2.php
@@ -0,0 +1,19 @@
+<html>
+    <head><title>Lab Task 3 - Problem 1</title></head>
+	<body>
+	    <?php
+		
+			$color = array('white', 'green', 'red', 'blue', 'black');
+		?> 
+			
+			<ul type="disk">
+				<li><?php echo $color[1] ?></li>
+				<li><?php echo $color[2] ?></li>
+			    <li><?php echo $color[0] ?></li>
+			</ul>	
+	
+        
+	     
+	</body>
+    
+</html>
