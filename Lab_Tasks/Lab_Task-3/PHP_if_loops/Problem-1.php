cf481c0da5b8f9ef689284502c70f1d6460172c6
EvanSarwer <evansarwer1@gmail.com>
2021-02-07 16:40:57 +0600

    PHP if loops prblm 1

---
+++ Lab_Tasks/Lab_Task-3/PHP_if_loops/Problem-1.php
@@ -0,0 +1,19 @@
+<html>
+    <head><title>Lab Task 3 - Problem 1</title></head>
+	<body>
+	    <?php
+		
+			$color = array('white', 'green', 'red', 'blue', 'black');
+		?> 
+			
+			<span>The memory of that scenefor me is like a frame of film forever frozen at <br>
+			that moment: the <span style="color:<?php echo $color[2] ?>">red</span> carpet, the <span style="color:<?php echo $color[1] ?>">green</span> lawn,
+			the <span style="color:<?php echo $color[0] ?>">white</span> house, the leaden sky. <br>
+			The new president and his first lady. 
+			- Richard M. Nixon </span>
+	
+        
+	     
+	</body>
+    
+</html>
